8fb854085fd591170454065a4e995875d10b898b
Tim Solley <tsolley@vergeops.com>
2019-09-03 15:24:00 -0400

    Added CA

---
+++ kops/autoscaling/apply_cluster_autoscaler.sh
@@ -0,0 +1,8 @@
+CLOUD_PROVIDER=aws
+IMAGE=k8s.gcr.io/cluster-autoscaler:v1.2.2
+MIN_NODES=1
+MAX_NODES=2
+AWS_REGION=us-west-2
+# For AWS GROUP_NAME should be the name of ASG as seen on AWS console
+GROUP_NAME="nodes.rvstore.k8s.local"
+SSL_CERT_PATH="/etc/ssl/certs/ca-certificates.crt" # (/etc/ssl/certs for gce, /etc/ssl/certs/ca-bundle.crt for RHEL7.X)
