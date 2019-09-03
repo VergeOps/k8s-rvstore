31cc9d02c6ac4935b4952dda0eb2f0a1d6eb5103
Tim Solley <tsolley@vergeops.com>
2019-09-03 15:32:21 -0400

    Fixed typos

--- kops/autoscaling/apply_cluster_autoscaler.sh
+++ kops/autoscaling/apply_cluster_autoscaler.sh
@@ -6,3 +6,5 @@ AWS_REGION=us-west-2
 # For AWS GROUP_NAME should be the name of ASG as seen on AWS console
 GROUP_NAME="nodes.rvstore.k8s.local"
 SSL_CERT_PATH="/etc/ssl/certs/ca-certificates.crt" # (/etc/ssl/certs for gce, /etc/ssl/certs/ca-bundle.crt for RHEL7.X)
+
+kubectl apply -f cluster_autoscaler.yaml
